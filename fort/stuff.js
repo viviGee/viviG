f48d3dd7ba5db97912d30567b959f6ae51518f7f
sugarcookiesoda <123298169+sugarcookiesoda@users.noreply.github.com>
2024-04-09 13:01:16 -0700

    Update stuff.js

--- fort/stuff.js
+++ fort/stuff.js
@@ -1,5 +1,5 @@
 // list of username thingies
-var adjectives = ['gay', 'sussy', 'epic', 'sus', 'homophobic', 'cool', 'homo', 'fat', 'sexy', 'dumb', 'ohio', 'skibidi', 'cute', 'kitten', 'fortnite', 'big', 'h', 'discord', 'roblox', 'anime', 'juicy', 'hot', 'DJ', 'stupid', 'dead', 'cubic', 'hard', 'murdered', 'OG', 'sigma', 'beta', 'alpha', 'mario', 'slutty', 'cum', 'edging', 'chris'];
+var adjectives = ['gay', 'sussy', 'epic', 'sus', 'homophobic', 'cool', 'homo', 'fat', 'sexy', 'dumb', 'ohio', 'skibidi', 'cute', 'kitten', 'fortnite', 'big', 'h', 'discord', 'roblox', 'anime', 'juicy', 'hot', 'DJ', 'stupid', 'dead', 'cubic', 'hard', 'murdered', 'OG', 'sigma', 'beta', 'alpha', 'mario', 'slutty', 'cum', 'edging', 'chris', 'edging', 'gay'];
 var nouns = ['gamer', 'rizzler', 'stripper', 'edger', 'monke', 'doge', 'sigma', 'thug', 'mod', 'kitten', 'hacker', 'admin', 'dog', 'sweat', 'monster', 'chungus', 'h', 'cum', 'earnings', 'CASH', 'jayden', 'greg', 'gerg', 'ben', 'david', 'mike', 'liam', 'hassewawa', 'dirt', 'drake', 'mason', 'jizz', 'cum', 'edger', 'gooner', 'bottle' , 'NERD', 'chan', 'sonichu'];
 var numbers = ['69', '420', '1337', '6', '2', '8', '4', '11', '2012', '2001', '1', '2', '3', '4', '5', '7', '9', '10', '44', '23', '14', '98', '‏‏‎ ‎', '32','111111', '999'];
 
